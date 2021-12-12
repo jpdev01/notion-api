74dd6be1a4c324ff4bc925b0e117cb58d38121ec
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2021-12-12 15:02:15 -0300

    User

--- src/main/java/com/jpdev/notion/core/domain/User.java
+++ src/main/java/com/jpdev/notion/core/domain/User.java
@@ -7,4 +7,41 @@ public class User extends BaseEntity {
 
     private String name;
 
+    private String email;
+
+    private String phone;
+
+    private String mobilePhone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
 }
