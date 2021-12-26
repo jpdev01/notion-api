f24de4654c805e52c4de29e0a246ddee216b2985
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2021-12-25 22:07:12 -0300

    EventDTO

--- src/main/java/com/jpdev/dto/EventDTO.java
+++ src/main/java/com/jpdev/dto/EventDTO.java
@@ -1,4 +1,14 @@
 package com.jpdev.dto;
 
+import java.util.List;
+
 public class EventDTO {
+
+    String startDate;
+
+    String finalDate;
+
+    String role;
+
+    List<String> participants;
 }
